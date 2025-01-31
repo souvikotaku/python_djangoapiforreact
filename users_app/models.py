97fc04d69be374b7a6b97f92e9fe589c40a88754
souvikotaku <manson1989@>
2025-01-31 19:28:38 +0530

    change in models.py

--- users_app/models.py
+++ users_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
 
