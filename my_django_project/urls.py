ef859de30977f80647fdd9dbd2606526156757fb
souvikotaku <manson1989@>
2025-01-31 17:41:41 +0530

    changes

--- my_django_project/urls.py
+++ my_django_project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users_app.urls')),  # Include users_app patterns once
 ]
+
+urlpatterns += staticfiles_urlpatterns()
